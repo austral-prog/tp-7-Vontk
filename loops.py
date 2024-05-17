ec72564cf3b4ed2b8ae64d2ccc17c1a056377ee2
Matias P. Menendez <121203087+Vontk@users.noreply.github.com>
2024-05-17 14:36:28 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,42 @@
+list_v1 = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+
+
 def index_of_by_index(word, list, index):
+    for i, element in enumerate(list):
+        if element == word:
+            if i >= index:
+                return i
     return -1
 
 
 def index_of_empty(list):
+    for index, element in enumerate(list):
+        if element == "":
+            return index
     return -1
 
 
 def index_of(word, list):
+    for index, search_word in enumerate(list):
+        if word in search_word:
+            return index
     return -1
 
 
 def put(word, list):
+    for index, element in enumerate(list):
+        if element == "":
+            del list[index]
+            list.insert(index, word)
+            return index
     return -1
 
 
 def remove(word, list):
-    return -1
+    n_of_deleted_words = 0
+    for i, e in enumerate(list):
+        if word in e:
+            del list[i]
+            list.insert(i, "")
+            n_of_deleted_words = n_of_deleted_words + 1
+    return n_of_deleted_words
